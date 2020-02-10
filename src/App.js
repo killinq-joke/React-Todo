e453355f82f4196e2340703a1140b6a8833912fc
Zak <zakadmaster@gmail.com>
2020-02-10 17:58:01 +0100

    Addition of some stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,80 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import uuid from 'uuid';
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+    constructor() {
+      super();
+      this.state = {
+          todos: [],
+          newTodoValue: '',
+
+      }
+  }
+
+  valueChange = event => {
+    const newValue = event.target.value;
+    console.log(event.target.value)
+    this.setState(oldState => {
+      return{
+        newTodoValue: newValue,
+      }
+    })
+  }
+
+  addTodo = event => {
+    console.log('click');
+    event.target.value = '';
+    this.setState(oldState => {
+      return{
+        newTodoValue: '',
+        todos: oldState.todos.concat({
+          completed: false,
+          id: uuid(),
+          task: oldState.newTodoValue
+        }),
+      }
+    })
+  }
+
+  // markCompleted = id => {
+    
+  //   this.setState(oldState => {
+  //     return {
+  //       todos: oldState.map(todo => {
+  //         todo
+  //       })
+        
+  //     }
+  //   })
+  // }
+
+  clearCompleted = event => {
+    this.setState(oldState => {
+          const arr = oldState.map();
+          console.log(arr);
+      return {
+        todos: arr.filter(todo => todo.completed === false)
+          
+        }
+      })
+  }
+
+//onClick={this.markCompleted(todo.id)}
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        {this.state.todos.map(todo => {
+          const color = todo.completed ? 'green' : 'red'
+          console.log(todo.task);
+          return(
+          <a style={{ color }}  key={todo.id}>{todo.task}</a>
+          )
+        })}
+        <input onChange={this.valueChange}></input>
+        <button onClick={this.addTodo}>Add Todo</button>
+        <button onClick={this.clearCompleted}>Clear Completed</button>
       </div>
     );
   }
